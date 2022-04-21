88a4cebd2e05db5d9aadecfdb32c29c8a027a5e2
Malcolm Ahoy <mahoy@ripple.com>
2022-04-21 14:58:35 -0400

    Add netlify function for xummPayment

---
+++ netlify/functions/xummPayment.ts
@@ -0,0 +1,24 @@
+import { Handler } from '@netlify/functions';
+import { Context } from '@netlify/functions/dist/function/context';
+import { Event } from '@netlify/functions/dist/function/event';
+import { XummSdk } from 'xumm-sdk';
+
+console.log('TEST LOG HERE', process.env);
+
+const Sdk = new XummSdk(process.env.XUMM_API_KEY, process.env.XUMM_API_SECRET);
+
+const handler: Handler = async (event: Event, context: Context) => {
+  if (event.httpMethod !== 'POST') {
+    return { statusCode: 405, body: 'Method Not Allowed' };
+  }
+
+  const pong = await Sdk.ping();
+
+  return {
+    statusCode: 200,
+    body: event.body,
+    randomStuff: pong,
+  };
+};
+
+export { handler };
